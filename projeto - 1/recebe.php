415fb277e609bc219401fb54fafde24a6255a6fc
geancorrea <geanbacinello197@gmail.com>
2024-12-17 10:59:20 -0300

    arquivos projeto 1
    arquivos projeto 1 html e css

---
+++ projeto - 1/recebe.php
@@ -0,0 +1,11 @@
+<?php
+# Isto é o início de um comentário
+# PHP tem três mães... 'C', shell-unix e Perl.
+# Form passa os dados digitados em VETORES PHP
+# Variável definida com "$" = $a="Casa";
+
+echo "<pre>";
+print_r($_REQUEST);  // Exibe de forma legível o conteúdo de $_REQUEST
+echo "</pre>";
+
+?>
